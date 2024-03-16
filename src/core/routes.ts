b7d4d2b16be64b29897414df9b4f92a1d2f63648
Yue Dong <ydong011@fiu.edu>
2024-03-16 01:04:00 -0500

    update

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express";
 import userRouters from "../auth/user.controller";
 import universityRouter from "../java/javahw.controller";
+import movieRouter from "../movies/movies.controller";
 
 export const routersConfig = (app: Express) => {
 	app.use(express.json());
 	app.use("/api/v1/auth", userRouters);
-	// app.use("/api/v1/movies");
+	// * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ movie router;
+	app.use("/api/v1/discover", movieRouter);
 	app.use("/api/v1/universities", universityRouter);
 };
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -4,14 +4,28 @@ import passport from "passport";
 import dotenv from "dotenv";
 
 dotenv.config();
-const userRouters = express.Router();
+const movieRouter = express.Router();
+
 const tmdbBaseUrl = process.env.TMDB_BASE_URL;
 const baseMovieImage = process.env.TMDB_BASE_MOVIE_IMG;
-const discoverMoviePath = "discover/movie?";
+const discoverMoviePath = "discover/movie";
 const searchMoviePath = "search/movie?";
 const discoverTvPath = "discover/tv?";
 const moviePath = "movie";
+const tmdb_key = process.env.TMDB_KEY;
 
-const convertGet: RequestHandler = (req, res) => {
+const getDiscoverMovie: RequestHandler = async (req, res) => {
 	const queryObj = { ...req.query } as { [key: string]: string };
+
+	const url = Object.entries(queryObj).reduce(
+		(acc, [key, value]) => `${acc}&${key}=${value}`,
+		`${tmdbBaseUrl}/${discoverMoviePath}?api_key=${tmdb_key}`
+	);
+
+	const result = await axios.get(url).then((ele) => ele.data);
+	res.status(201).json(result);
 };
+
+movieRouter.route("/movie").get(getDiscoverMovie);
+
+export default movieRouter;
